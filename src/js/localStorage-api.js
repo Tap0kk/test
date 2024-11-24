200a0ac0ecfda836b5af52e8b3d6400a6d190241
Deman <deman2805@gmail.com>
2024-11-24 09:32:17 +0200

    1

---
+++ src/js/localStorage-api.js
@@ -0,0 +1,26 @@
+
+export function addDataToLocalStorage(key, value) {
+    try {
+      const normolizedState = JSON.stringify(value);
+      localStorage.setItem(key, normolizedState);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export function getDataLocalStorage(key) {
+    try {
+      const normolizedState = localStorage.getItem(key);
+      return normolizedState === null ? undefined : JSON.parse(normolizedState);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export function removeDataLocalStorage(key) {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.log(error);
+    }
+  }
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/mobile-menu.js';
+import './js/switch-theme.js';
+import './js/my-projects.js';
+import './js/faq-accordeon.js';
+import './js/reviews.js';
+import './js/form.js';
+import './js/modal.js';
