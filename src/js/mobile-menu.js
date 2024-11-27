64c241504de782ef76dbcec5ed70db96a72410ae
Deman <deman2805@gmail.com>
2024-11-27 19:20:25 +0200

    change header

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,18 +1,18 @@
-const menuBtn = document.querySelector('.js-menu-open');
+const btnMobMenu = document.querySelector('.js-menu-open');
 const mobileMenu = document.querySelector('.js-mobile-menu');
 const closeBtn = document.querySelector('.js-mobile-menu-close');
 const menuLinks = document.querySelectorAll('.js-nav-link');
 
 window.addEventListener('DOMContentLoaded', () => {
-  menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('active');
-    document.body.classList.add('no-scroll');
+  btnMobMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
   });
 
-  closeBtn.addEventListener('click', () => {
-    mobileMenu.classList.remove('active');
-    document.body.classList.remove('no-scroll');
-  });
+  //   closeBtn.addEventListener('click', () => {
+  //     mobileMenu.classList.remove('active');
+  //     document.body.classList.remove('no-scroll');
+  //   });
 
   menuLinks.forEach(link => {
     link.addEventListener('click', () => {
@@ -20,4 +20,9 @@ window.addEventListener('DOMContentLoaded', () => {
       document.body.classList.remove('no-scroll');
     });
   });
+});
+const menuBtn = document.querySelector('.mob-menu-btn');
+const openBtn = document.querySelector('.menu-btn');
+menuBtn.addEventListener('click', function () {
+  openBtn.classList.toggle('open');
 });
